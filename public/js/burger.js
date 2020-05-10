546b79857a3226e2f3695d030023bf16ebc1c1ca
John O'Neil <fluffywestie@tpg.com.au>
2020-05-10 21:31:27 +1000

    AJAX Methods

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
 
     var burgers = [];
-    // $("#undevourdList").append('Undevoured goes here!');
-    // $("#devouredList").append('Devoured goes here!');
 
     const unDevouredList = $("#undevouredList");
     const DevouredList = $("#devouredList");
 
+    //Event Handler
+    $(document).on("click", "button.devour", devourBurger);
+    $(document).on("click", "button.addburger", addBurger);
 
     function getBurgers() {
 
@@ -19,8 +20,9 @@ $(document).ready(function () {
             burgers.forEach(element => {
                 var newListItem = $("<li>")
                 var newButton = $("<button>");
+                newButton.text(element.burger_name);
+                newButton.addClass("devour btn btn-primary");
 
-                newButton.addClass("btn btn-primary");
                 newButton.prop('value', element.id);
 
                 newListItem.append(newButton);
@@ -36,28 +38,52 @@ $(document).ready(function () {
             // Now generate buttons representing the burgers
             burgers.forEach(element => {
                 var newListItem = $("<li>")
-                var newButton = $("<button>");
-
                 newButton.addClass("btn btn-primary");
                 newButton.prop('value', element.id);
-                newButton.innerHTML("BUTTON");
+                newButton.attr('text', element.burger_name);
 
-                newListItem.append(newButton);
+                newButton.on("click", function () {
+                    console.log('click detected');
+                });
+
                 DevouredList.append(newListItem);
             });
         });
-
-        // UnDevoured Burgers List
-        // Add Event on Button
-        // Event is AJAX call to server 
-
-        // Devoured Burgers List
     }
 
     // Add event for 'add burger to undevoured list'
+    function devourBurger() {
+        var burgerID = $(this).attr('value');
+        alert(`Devouring burger ${burgerID}`);
+    }
+
+    function addBurger() {
+
+        var burgerName = "";
+
+        if ($("#newburger option:selected").text() === "Please Select a Sandwich") {
+            console.log('No action taken, nothing selected');
+        } else {
+            burgerName = $("#newburger option:selected").text();
+            console.log(burgerName);
+            var newBurger = {
+                "burger_name": burgerName,
+                "devoured": 0
+            }
+            console.log("New burger" + JSON.stringify(newBurger) +
+                $("#newburger option:selected").text());
+
+            $.post("/api/burgers", newBurger, function (result) {
+                console.log(result);
+            });
+        }
+        // re-render page
+        window.location.reload();
+    }
 
     // Add event for 'Delete Devoured Burger'
 
 
+    // Iniital Run logic
     getBurgers();
 });
