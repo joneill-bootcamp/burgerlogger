9587c15b2c2914c4f582772e518403358051fa42
John O'Neil <fluffywestie@tpg.com.au>
2020-05-10 19:51:46 +1000

    Updated Widgets

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,30 +1,63 @@
 $(document).ready(function () {
 
-    var burgerContainer = $("");
-    var burgers;
+    var burgers = [];
+    // $("#undevourdList").append('Undevoured goes here!');
+    // $("#devouredList").append('Devoured goes here!');
 
-    $("#undevourdList").append('Undevoured goes here!');
-    $("#devouredList").append('Devoured goes here!');
+    const unDevouredList = $("#undevouredList");
+    const DevouredList = $("#devouredList");
 
-    function clearLists() {
-        $("#undevourdList").clear();
-        $("#devouredList").clear();
-    }
 
-    function renderLists(burgers) {
+    function getBurgers() {
+
+        $.get("/api/burgers/?devoured=0", function (data) {
+            burgers = data;
+
+            console.log("UnDevoured Burgers Data", burgers);
+
+            // Now generate buttons representing the burgers
+            burgers.forEach(element => {
+                var newListItem = $("<li>")
+                var newButton = $("<button>");
+
+                newButton.addClass("btn btn-primary");
+                newButton.prop('value', element.id);
+
+                newListItem.append(newButton);
+                unDevouredList.append(newListItem);
+            });
+        });
+
+        $.get("/api/burgers/?devoured=1", function (data) {
+            burgers = data;
+
+            console.log("Devoured Burgers Data", burgers);
+
+            // Now generate buttons representing the burgers
+            burgers.forEach(element => {
+                var newListItem = $("<li>")
+                var newButton = $("<button>");
+
+                newButton.addClass("btn btn-primary");
+                newButton.prop('value', element.id);
+                newButton.innerHTML("BUTTON");
+
+                newListItem.append(newButton);
+                DevouredList.append(newListItem);
+            });
+        });
 
         // UnDevoured Burgers List
-        // Add Button
         // Add Event on Button
         // Event is AJAX call to server 
 
         // Devoured Burgers List
-
-
     }
 
     // Add event for 'add burger to undevoured list'
 
     // Add event for 'Delete Devoured Burger'
 
+
+    getBurgers();
 });
